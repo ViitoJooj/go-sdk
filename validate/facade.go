db542750bc540f934ba6b147e0e4deea0b183389
ViitoJooj <joaovitor819oqueres@gmail.com>
2026-08-01 15:08:21 -0300

    feat(validate): add Brasil API CNPJ validation

--- validate/facade.go
+++ validate/facade.go
@@ -2,6 +2,7 @@ package validate
 
 import (
 	"database/sql"
+	"net/http"
 
 	"github.com/ViitoJooj/go-sdk/validate/addresses"
 	"github.com/ViitoJooj/go-sdk/validate/company"
@@ -31,8 +32,10 @@ func District(district string) error       { return addresses.District(district)
 func City(city string) error               { return addresses.City(city) }
 func StateRegion(stateRegion string) error { return addresses.StateRegion(stateRegion) }
 
-func CNPJ(cnpj string) error                   { return company.CNPJ(cnpj) }
-func CorporateName(corporateName string) error { return company.CorporateName(corporateName) }
+func CNPJ(cnpj string) error                              { return company.CNPJ(cnpj) }
+func CNPJWithBrasilAPI(cnpj string) error                  { return company.CNPJWithBrasilAPI(cnpj) }
+func CNPJWithBrasilAPIClient(cnpj string, c *http.Client) error { return company.CNPJWithBrasilAPIClient(cnpj, c) }
+func CorporateName(corporateName string) error            { return company.CorporateName(corporateName) }
 func TradeName(tradeName string) error         { return company.TradeName(tradeName) }
 func IM(im string) error                       { return company.IM(im) }
 func IE(ie string) error                       { return company.IE(ie) }
